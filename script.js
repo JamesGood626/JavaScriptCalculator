1adb8880e7db1f1c3a29d6a68e9ffdbff5a2cc36
James <jamesgood626@gmail.com>
2017-06-15 01:21:04 -0700

    Yesh. Added calculation functionality. And it clears the array in all possible scenarios. Haven't been able to break it yet. Just need to add decimal, +/-, and % capability in the future, but great progress has been made.

--- script.js
+++ script.js
@@ -9,24 +9,43 @@ var calculatorInput = {
   target.innerHTML = calcInput;
     console.log(arr.arr1);
   },
+  resetArray: () => {
+    for(let i = arr.arr1.length; i > 0; i--) {
+      arr.arr1.pop();
+    }
+    var target = document.getElementById("calc-input");
+    target.innerHTML = '';
+  },
   //Strange... function operates fine with regular function declaration, but if I use the arrow syntax then this.displayArr(); must be changed to calculatorInput.displayArr(); Look into.
   enterNumber: function(calcNum) {
-    if(arr.arr1.length === 2 && (arr.arr1[1] === "+" || arr.arr1[1] === "-" || arr.arr1[1] === "x" || arr.arr1[1] === "/"))
-      {
-         arr.arr1.push(calcNum);
+    if(arr.arr1.length === 0 || arr.arr1.length === 2) {
+      arr.arr1.push(calcNum);
+      console.log("result of first if statement");
+      this.displayArr();
       }
-    else if(arr.arr1.length > 0 || arr.arr1.length > 3) {
+    else if(arr.arr1.length > 3 || arr.arr1.length === 1) {
         for(let i = arr.arr1.length; i > 0; i--) {
           arr.arr1.pop();
         }
+           arr.arr1.push(calcNum);
+      this.displayArr();
       }
-    else {
-      arr.arr1.push(calcNum);
-      }
-    this.displayArr();
   },
   enterOperator: function(operator) {
       arr.arr1.push(operator);
+  },
+  calculateArray: function(calculate) {
+    var total = eval(calculate.join(''));
+    console.log(total);
+    for(let i = arr.arr1.length; i > 0; i--) {
+          arr.arr1.pop();
+          console.log("result of calculateArray statement");
+        }
+    var calcInput = arr.arr1[arr.arr1.length - 1];
+    var target = document.getElementById("calc-input");
+    target.innerHTML = total;
+    arr.arr1.push(total);
+
   }
 
 }
@@ -41,9 +60,24 @@ calculatorElement.addEventListener("click", function(e) {
       var numToEnter = calcBtnPress;
       calculatorInput.enterNumber(numToEnter);
     }
-    else if(calcBtnPress === '÷' || calcBtnPress === 'x' || calcBtnPress === '-' || calcBtnPress === '+' && arr.arr1.length === 1) {
-      var operator = calcBtnPress;
-      calculatorInput.enterOperator(operator);
+    else if((calcBtnPress === '÷' || calcBtnPress === 'x' || calcBtnPress === '-' || calcBtnPress === '+')
+            && arr.arr1.length === 1) {
+        if(calcBtnPress === 'x') {
+          calcBtnPress = '*';
+        }
+        else if(calcBtnPress === '÷') {
+          calcBtnPress = '/';
+        }
+        var operator = calcBtnPress;
+        calculatorInput.enterOperator(operator);
+    }
+    else if(calcBtnPress === '=' && arr.arr1.length === 3) {
+      console.log("= has been pressed");
+      var array = arr.arr1;
+      calculatorInput.calculateArray(array);
+    }
+    else if(calcBtnPress === 'C') {
+      calculatorInput.resetArray();
     }
   }
 }, false);
